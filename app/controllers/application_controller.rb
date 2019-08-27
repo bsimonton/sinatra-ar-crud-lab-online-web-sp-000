09f7259d797a502b1effefc5dd126618e4a1b2b8
Brian Simonton <bsimonton78@gmail.com>
2019-08-27 00:05:18 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,10 @@ class ApplicationController < Sinatra::Base
     erb :new
   end 
   
-  
+   post '/articles' do
+    @article = Article.create(:title => params[:title], :content => params[:content])
+    redirect to "/articles/#{@article.id}"
+  end
   
   
   get '/articles' do 
